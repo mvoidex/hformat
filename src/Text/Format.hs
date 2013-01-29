412f14b4e954555f70f9a6b11e0292c3c9eb270c
mvoidex <voidex@live.com>
2013-01-30 03:21:06 +0400

    Added recursive args

--- src/Text/Format.hs
+++ src/Text/Format.hs
@@ -1,20 +1,23 @@
 {-# LANGUAGE DefaultSignatures, FlexibleInstances #-}
 
 module Text.Format (
+    FormatValue(..),
     FormatArg, FormatArgs,
     FormatShow(..),
     format,
-    (%=)
+    (%=), (%%)
     ) where
 
 import Data.Char (isAlpha)
 import Data.Maybe (fromMaybe)
 import Data.Text (Text, pack, unpack)
 
-type FormatArg = (String, Text)
+data FormatValue = FormatString String | FormatPure Text
+
+type FormatArg = (String, FormatValue)
 type FormatArgs = [FormatArg]
 
--- | format "My name is $name, I am $age years old" $ ["name" %= "Jack", "age" %%= 24]
+-- | format "My name is $name, I am $age years old" $ ["name" %= "Jack", "age" %= 24]
 format :: String -> FormatArgs -> Text
 format fmt args = pack $ format' fmt args where
     format' :: String -> FormatArgs -> String
@@ -22,7 +25,9 @@ format fmt args = pack $ format' fmt args where
     format' ('$':('$':fmt)) args = '$' : format' fmt args
     format' ('$':fmt) args = case name of
         "" -> '$' : format' fmt args
-        _ -> maybe (error $ "Format argument " ++ name ++ " expected") unpack (lookup name args) ++ format' fmt' args
+        _ -> case fromMaybe (error $ "Format argument '" ++ name ++ "' expected") (lookup name args) of
+            FormatString f -> format' (f ++ fmt') args
+            FormatPure s -> unpack s ++ format' fmt' args
         where
         (name, fmt') = span isAlpha fmt
     format' (c:fmt) args = c : format' fmt args
@@ -49,7 +54,12 @@ instance FormatShow Text where
     formatShow = id
 
 infixr 1 %=
+infixr 1 %%
 
 -- | Used to form argument
 (%=) :: FormatShow a => String -> a -> FormatArg
-name %= value = (name, formatShow value)
+name %= value = (name, FormatPure $ formatShow value)
+
+-- | Form argument as format string
+(%%) :: String -> String -> FormatArg
+name %% fmt = (name, FormatString fmt)
